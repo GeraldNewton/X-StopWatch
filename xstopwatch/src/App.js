0f1b9a70831f21a5daa8990f4fe9d4fda1508927
GeraldNewton <arpitnewton1@gmail.com>
2024-02-26 14:07:49 +0530

    second

--- xstopwatch/src/App.js
+++ xstopwatch/src/App.js
@@ -1,46 +1,104 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+
+// function App() {
+//   let [time, setTime] = useState(0);
+//   let [isRunning, setIsRunning] = useState(false);
+
+//   useEffect(() => {
+//     let timeID;
+//     if (isRunning) {
+//       timeID = setInterval(() => {
+//         setTime((prevState) => prevState + 1);
+//       }, 1000);
+//     }
+//     return () => clearInterval(timeID);
+//   }, [isRunning]);
+
+//   let format = (time) => {
+//     let sec = time % 60;
+//     let min = time / 60;
+//     console.log(Math.floor(min));
+//     return `${Math.floor(min)}:${resolve(sec)}`;
+//   };
+
+//   let resolve = (sec) => {
+//     if (sec < 10) {
+//       return `0${sec}`;
+//     } else return sec;
+//   };
+
+//   let handleClickReset = () => {
+//     setTime(0);
+//     setIsRunning(false);
+//   };
+
+//   let handleClick = () => setIsRunning((prevState) => !prevState);
+
+//   return (
+//     <div>
+//       <h1>Stopwatch</h1>
+//       <p>Time : {format(time)}</p>
+//       <button onClick={handleClick}>{isRunning ? `Stop` : `Start`}</button>
+//       <button onClick={handleClickReset}>Reset</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, { useState, useRef } from 'react';
+import './App.css';
 
 function App() {
-  let [time, setTime] = useState(0);
-  let [isRunning, setIsRunning] = useState(false);
+  const [time, setTime] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const intervalRef = useRef(null);
 
-  useEffect(() => {
-    let timeID;
-    if (isRunning) {
-      timeID = setInterval(() => {
-        setTime((prevState) => prevState + 1);
-      }, 1000);
-    }
-    return () => clearInterval(timeID);
-  }, [isRunning]);
-
-  let format = (time) => {
-    let sec = time % 60;
-    let min = time / 60;
-    return `${Math.floor(min)}:${resolve(sec)}`;
+  const formatTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   };
 
-  let resolve = (sec) => {
-    if (sec < 10) {
-      return `0${sec}`;
-    } else return sec;
+  const startTimer = () => {
+    setIsRunning(true);
+    intervalRef.current = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
   };
 
-  let handleClickReset = () => {
-    setTime(0);
+  const stopTimer = () => {
     setIsRunning(false);
+    clearInterval(intervalRef.current);
   };
 
-  let handleClick = () => setIsRunning((prevState) => !prevState);
+  const resetTimer = () => {
+    stopTimer();
+    setTime(0);
+  };
+
+  const handleStartStop = () => {
+    if (isRunning) {
+      stopTimer();
+    } else {
+      startTimer();
+    }
+  };
 
   return (
-    <div>
+    <div className="App">
       <h1>Stopwatch</h1>
-      <p>Time : {format(time)}</p>
-      <button onClick={handleClick}>{isRunning ? `Stop` : `Start`}</button>
-      <button onClick={handleClickReset}>Reset</button>
+      <div className="time">{formatTime(time)}</div>
+      <div className="buttons">
+        <button onClick={handleStartStop}>
+          {isRunning ? 'Stop' : 'Start'}
+        </button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
