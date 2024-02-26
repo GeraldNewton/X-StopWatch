4b0ab05e6491d1d917a6d4a1a4508ea5b656cc49
GeraldNewton <arpitnewton1@gmail.com>
2024-02-26 14:44:45 +0530

    second

--- xstopwatch/src/App.js
+++ xstopwatch/src/App.js
@@ -57,7 +57,7 @@ function App() {
   const formatTime = (time) => {
     const minutes = Math.floor(time / 60);
     const seconds = time % 60;
-    return `${minutes} : ${seconds < 10 ? '0' : ''}${seconds}`;
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
   };
 
   const startTimer = () => {
